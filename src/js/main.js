6240d493872c408ae540b25076dc69033f1b6683
Rin <kia01182008@gmail.com>
2026-05-24 12:28:05 +0200

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,16 @@
 import Handlebars from 'handlebars';
 import { products } from '../../data.js';
-import productTemplateSource from '../templates/product.hbs';
 
-// Compile the .hbs template
+const productTemplateSource = `
+<div class="product-card">
+  <h2 class="product-name">{{name}}</h2>
+  <p class="product-description">{{description}}</p>
+  <span class="product-price">\${{price}}</span>
+</div>
+`;
+
 const productTemplate = Handlebars.compile(productTemplateSource);
 
-// Render each product and inject into DOM
 const container = document.getElementById('products');
 
 products.forEach(product => {
